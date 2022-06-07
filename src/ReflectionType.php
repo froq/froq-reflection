5872c2f02fa43f9234d3278a3a54b9a7ebf5650c
Kerem Güneş <qeremy@gmail.com>
2022-06-07 18:19:06 +0300

    Add ReflectionType class.

---
+++ src/ReflectionType.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-reflection
+ */
+declare(strict_types=1);
+
+namespace froq\reflection;
+
+/**
+ * An extended `ReflectionType` class, combines `ReflectionNamedType`,
+ * `ReflectionUnionType` and `ReflectionIntersectionType` as one.
+ *
+ * @package froq\reflection
+ * @object  froq\reflection\ReflectionType
+ * @author  Kerem Güneş
+ * @since   5.31, 6.0
+ */
+class ReflectionType extends \ReflectionType implements \Reflector
+{
+    /** Name/nullable reference. */
+    private object $reference;
+
+    /** Typing delimiter. */
+    private string $delimiter;
+
+    /**
+     * Constructor.
+     *
+     * @param  string $name
+     * @param  bool   $nullable
+     * @throws ReflectionException
+     */
+    public function __construct(string $name, bool $nullable = false)
+    {
+        $name || throw new \ReflectionException('No name given');
+
+        // Null/mixed is nullable.
+        if ($name == 'null' || $name == 'mixed') {
+            $nullable = true;
+        }
+
+        // Uniform nullable types.
+        if ($name[0] == '?') {
+            $name = substr($name, 1);
+            $nullable = true;
+        }
+
+        // @tome: Null allways goes to the end.
+        if ($nullable && ($name != 'null' && $name != 'mixed')) {
+            $name .= '|null';
+        }
+
+        // @tome: Intersection-type not allows nulls.
+        $this->delimiter = str_contains($name, '&') ? '&' : '|';
+
+        $name = implode($this->delimiter, array_unique(explode($this->delimiter, $name)));
+
+        $this->reference = (object) ['name' => $name, 'nullable' => $nullable];
+    }
+
+    /**
+     * Proxy for reference object properties.
+     *
+     * @param  string $property
+     * @return string|bool
+     * @throws ReflectionException
+     * @magic
+     */
+    public function __get(string $property): string|bool
+    {
+        if (isset($this->reference->$property)) {
+            return $this->reference->$property;
+        }
+
+        throw new \ReflectionException(sprintf(
+            'Undefined property %s::$%s', $this::class, $property
+        ));
+    }
+
+    /** @magic */
+    public function __debugInfo(): array
+    {
+        return ['name' => $this->reference->name,
+                'nullable' => $this->reference->nullable];
+    }
+
+    /** @magic */
+    public function __toString(): string
+    {
+        return $this->reference->name;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->reference->name;
+    }
+
+    /**
+     * Get pure name if named (without "null" part if nullable).
+     *
+     * @return string|null
+     */
+    public function getPureName(): string|null
+    {
+        return $this->isNamed() ? $this->getNames()[0] : null;
+    }
+
+    /**
+     * Get names.
+     *
+     * @return array
+     */
+    public function getNames(): array
+    {
+        return explode($this->delimiter, $this->reference->name);
+    }
+
+    /**
+     * Get types.
+     *
+     * @return array<froq\reflection\ReflectionType>
+     */
+    public function getTypes(): array
+    {
+        return array_map(fn($name) => new ReflectionType($name), $this->getNames());
+    }
+
+    /**
+     * Builtin state checker.
+     *
+     * @return bool
+     */
+    public function isBuiltin(): bool
+    {
+        return preg_test('~^(int|float|string|bool|array|object|callable|iterable|mixed)(\|null)?$~',
+            $this->getName());
+    }
+
+    /**
+     * Named-type state checker.
+     *
+     * @return bool
+     */
+    public function isNamed(): bool
+    {
+        return !$this->isUnion() && !$this->isIntersection();
+    }
+
+    /**
+     * Union-type state checker.
+     *
+     * @return bool
+     */
+    public function isUnion(): bool
+    {
+        return substr_count($this->getName(), '|') >= 2;
+    }
+
+    /**
+     * Intersection-type state checker.
+     *
+     * @return bool
+     */
+    public function isIntersection(): bool
+    {
+        return substr_count($this->getName(), '&') >= 1;
+    }
+
+    /**
+     * Nullable state checker.
+     *
+     * @return bool
+     */
+    public function isNullable(): bool
+    {
+        return $this->reference->nullable;
+    }
+
+    /**
+     * @alias isNullable()
+     */
+    public function allowsNull(): bool
+    {
+        return $this->reference->nullable;
+    }
+
+    /**
+     * Check whether contains given type name.
+     *
+     * @param  string $name
+     * @return bool
+     */
+    public function contains(string $name): bool
+    {
+        return in_array($name, $this->getNames(), true);
+    }
+}
