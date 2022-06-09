d7e836fadf379ca3d78391061bb97497c0b4c7fd
Kerem Güneş <qeremy@gmail.com>
2022-06-09 18:12:21 +0300

    Add reflector.ParentReflector class.

---
+++ src/reflector/ParentReflector.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-reflection
+ */
+declare(strict_types=1);
+
+namespace froq\reflection\reflector;
+
+use froq\reflection\ReflectionClass;
+use froq\util\Objects;
+
+/**
+ * Parent (class) reflector class.
+ *
+ * @package froq\reflection\reflector
+ * @object  froq\reflection\reflector\ParentReflector
+ * @author  Kerem Güneş
+ * @since   6.0
+ */
+class ParentReflector extends Reflector
+{
+    /**
+     * Set of parents.
+     *
+     * @return Set<froq\reflection\ReflectionClass>
+     */
+    public function parents(): \Set
+    {
+        return (new \Set($this->getParentNames()))
+            ->map(fn($name) => new ReflectionClass($name));
+    }
+
+    /**
+     * Get parent.
+     *
+     * @return froq\reflection\ReflectionClass|null
+     */
+    public function getParent(): ReflectionClass|null
+    {
+        $ret = $this->getParentName();
+
+        return $ret ? new ReflectionClass($ret) : null;
+    }
+
+    /**
+     * Get parents.
+     *
+     * @return array<froq\reflection\ReflectionClass>
+     */
+    public function getParents(): array
+    {
+        $ret = $this->getParentNames();
+
+        return $ret ? array_map(fn($name) => new ReflectionClass($name), $ret) : $ret;
+    }
+
+    /**
+     * Get parent name.
+     *
+     * @return string
+     */
+    public function getParentName(): string
+    {
+        return (string) Objects::getParent($this->reflector->name);
+    }
+
+    /**
+     * Get parent names.
+     *
+     * @return array<string>
+     */
+    public function getParentNames(): array
+    {
+        return (array) Objects::getParents($this->reflector->name);
+    }
+}
