c653610d76e391aa47f0e3341a0d93676a59bfbc
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-11-03 01:08:26 +0300

    - ReflectionFunction: add __construct(), use ReferenceTrait.

--- src/ReflectionFunction.php
+++ src/ReflectionFunction.php
@@ -1,11 +1,11 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright (c) 2015 · Kerem Güneş
  * Apache License 2.0 · http://github.com/froq/froq-reflection
  */
-declare(strict_types=1);
-
 namespace froq\reflection;
+
+use froq\reflection\internal\trait\{CallableTrait, ReferenceTrait};
 
 /**
  * An extended `ReflectionFunction` class.
@@ -17,5 +17,23 @@ namespace froq\reflection;
  */
 class ReflectionFunction extends \ReflectionFunction
 {
-    use internal\trait\CallableTrait;
+    use CallableTrait, ReferenceTrait;
+
+    /**
+     * Constructor.
+     *
+     * @param string|Closure $function
+     */
+    public function __construct(string|\Closure $function)
+    {
+        parent::__construct($function);
+
+        // Create internal reflection.
+        $reflection = new \ReflectionFunction($function);
+
+        $this->setReference([
+            'callable'   => $function,
+            'reflection' => $reflection
+        ]);
+    }
 }
