a9224bc045a1d3c2ce75dd2862bf770fb013834c
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-11-03 20:54:39 +0300

    ReflectionParameter:   - Add __construct() [to allow methods as "Foo::bar"].   - Add getClass(),getDeclaringClass(),getDeclaringFunction() [override].   - Add getDeclaringMethod().   - Add isNullable().   - Update getTypes() [change return null => array].   - Doc-ups & make-ups.

--- src/ReflectionParameter.php
+++ src/ReflectionParameter.php
@@ -18,6 +18,95 @@ namespace froq\reflection;
 class ReflectionParameter extends \ReflectionParameter
 {
     /**
+     * Constructor.
+     *
+     * @param string|array|object $functionOrMethod
+     * @param string|int          $nameOrPosition
+     */
+    public function __construct(string|array|object $functionOrMethod, int|string $nameOrPosition)
+    {
+        if ( // When "Foo::bar" given as method.
+            is_string($functionOrMethod)
+            && preg_match('~(.+)::(\w+)~', $functionOrMethod, $match)
+        ) {
+            $functionOrMethod = array_slice($match, 1);
+        }
+
+        parent::__construct($functionOrMethod, $nameOrPosition);
+    }
+
+    /**
+     * Get class name (if its function is a method).
+     *
+     * Note: This method is deprecated by the internal reflection API,
+     * but we continue to provide it though.
+     *
+     * @return string|null
+     * @override
+     */
+    #[\ReturnTypeWillChange]
+    public function getClass(): string|null
+    {
+        return parent::getDeclaringClass()?->name;
+    }
+
+    /**
+     * Get declaring class.
+     *
+     * @return froq\reflection\ReflectionClass|null
+     * @override
+     */
+    public function getDeclaringClass(): ReflectionClass|null
+    {
+        $ref = parent::getDeclaringClass();
+        if (!$ref) {
+            return null;
+        }
+
+        return match (true) {
+            default => new ReflectionClass($ref->name),
+            $ref?->isTrait() => new ReflectionTrait($ref->name),
+            $ref?->isInterface() => new ReflectionInterface($ref->name),
+        };
+    }
+
+    /**
+     * Get declaring method (if its function is a method).
+     *
+     * @return froq\reflection\ReflectionMethod|null
+     */
+    public function getDeclaringMethod(): ReflectionMethod|null
+    {
+        $ref = parent::getDeclaringFunction();
+
+        if ($ref && $ref instanceof \ReflectionMethod) {
+            return new ReflectionMethod($ref->class, $ref->name);
+        }
+
+        return null;
+    }
+
+    /**
+     * Get declaring function.
+     *
+     * @return froq\reflection\{ReflectionMethod|ReflectionFunction}|null
+     * @override
+     */
+    #[\ReturnTypeWillChange]
+    public function getDeclaringFunction(): ReflectionMethod|ReflectionFunction|null
+    {
+        if ($ref = $this->getDeclaringMethod()) {
+            return $ref;
+        }
+
+        if ($ref = parent::getDeclaringFunction()) {
+            return new ReflectionFunction($ref->getClosure());
+        }
+
+        return null;
+    }
+
+    /**
      * Check default value existence.
      *
      * @return bool
@@ -31,25 +120,32 @@ class ReflectionParameter extends \ReflectionParameter
      * Get default value return null.
      *
      * @return mixed
+     * @override
      */
     public function getDefaultValue(): mixed
     {
         return $this->hasDefaultValue() ? parent::getDefaultValue() : null;
     }
 
-    /** @override */
+    /**
+     * @override
+     */
     public function isDefaultValueConstant(): bool
     {
         return $this->hasDefaultValue() ? parent::isDefaultValueConstant() : false;
     }
 
-    /** @override */
+    /**
+     * @override
+     */
     public function getDefaultValueConstantName(): string|null
     {
         return $this->hasDefaultValue() ? parent::getDefaultValueConstantName() : null;
     }
 
-    /** @override */
+    /**
+     * @override
+     */
     public function getType(): ReflectionType|null
     {
         if ($type = parent::getType()) {
@@ -59,12 +155,20 @@ class ReflectionParameter extends \ReflectionParameter
     }
 
     /**
-     * Get types (if available).
+     * Get types.
      *
-     * @return array<froq\reflection\ReflectionType>|null
+     * @return array<froq\reflection\ReflectionType>
      */
-    public function getTypes(): array|null
+    public function getTypes(): array
     {
-        return $this->getType()?->getTypes();
+        return (array) $this->getType()?->getTypes();
+    }
+
+    /**
+     * @alias allowsNull()
+     */
+    public function isNullable(): bool
+    {
+        return $this->allowsNull();
     }
 }
