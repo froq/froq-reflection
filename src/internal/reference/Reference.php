1f455c9c399f940956205e4bae3fb6b048d0b09a
Kerem Güneş <1508306+k-gun@users.noreply.github.com>
2022-11-03 23:47:55 +0300

    Docs-ups.

--- src/internal/reference/Reference.php
+++ src/internal/reference/Reference.php
@@ -1,6 +1,18 @@
 <?php declare(strict_types=1);
-
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-reflection
+ */
 namespace froq\reflection\internal\reference;
 
+/**
+ * Base reference class.
+ *
+ * @package froq\reflection\internal\reference
+ * @class   froq\reflection\internal\reference\Reference
+ * @author  Kerem Güneş
+ * @since   7.0
+ * @internal
+ */
 class Reference
 {}
